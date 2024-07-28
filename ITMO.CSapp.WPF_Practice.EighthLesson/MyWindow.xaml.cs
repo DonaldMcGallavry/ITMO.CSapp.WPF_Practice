c31fca66ea5e9d3686d3d9e4019fdf9c28ab57d6
N.Dubskiy <ndubskiy@mail.ru>
2024-07-28 15:56:36 +0300

    Commit

---
+++ ITMO.CSapp.WPF_Practice.EighthLesson/MyWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+
+
+
+namespace ITMO.CSapp.WPF_Practice.EighthLesson
+{
+
+    /// <summary>
+    /// Interaction logic for MyWindow.xaml
+    /// </summary>
+    public partial class MyWindow : Window
+    {
+        private bool _close;
+        MainWindow wnd1 = null;
+
+
+        public MyWindow()
+        {
+            InitializeComponent();
+           
+        }
+        public new void Close() 
+        {
+            _close = true; 
+            base.Close(); 
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_close) return;
+            e.Cancel = true; 
+            Hide();
+        }
+
+        private void ButLoad_Click(object sender, RoutedEventArgs e)
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("D:\\CourseSamples\\C#Dev\\ITMO.CSapp.WPF_Practice\\ITMO.CSapp.WPF_Practice.EighthLesson\\log.txt", true))
+            {
+                writer.WriteLine("Внесено {0}: {1} ", textBox.Text, DateTime.Now.ToShortDateString() + ", время: " +
+                DateTime.Now.ToLongTimeString());
+                writer.Flush();
+            }
+
+            wnd1 = Owner as MainWindow;
+            if (wnd1 != null && listBox1.SelectedItem is Student selectedStudent)
+            {
+                wnd1.txtBlock.Text = textBox.Text;
+                string studentInfo = selectedStudent.FullStudentData;
+                wnd1.txtBlock.Text = $"Студент: {studentInfo}";
+            }
+            Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            wnd1.myWin = null;
+        }
+
+    }
+}
---
+++ ITMO.CSapp.WPF_Practice.ThirdLesson.Exercise3/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITMO.CSapp.WPF_Practice.ThirdLesson.Exercise3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string nameFile = "D:\\CourseSamples\\C#Dev\\ITMO.CSapp.WPF_Practice\\ITMO.CSapp.WPF_Practice.ThirdLesson.Exercise3\\username.txt";
+
+        bool isDataDirty = false;
+        public MyWindow myWin { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbl.Content = "Добрый день!";
+            setBut.IsEnabled = false;
+            retBut.IsEnabled = false;
+        }
+
+        private void SetBut()
+        {
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(nameFile);
+            sw.WriteLine(setText.Text);
+            sw.Close();
+            retBut.IsEnabled = true;
+            isDataDirty = false;
+        }
+
+        private void RetBut()
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader(nameFile);
+            retLabel.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+            sr.Close();
+           
+        }
+
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            setBut.IsEnabled = true;
+            isDataDirty = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения?";
+
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Warning);
+                if
+                    (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void New_Win_Click(object sender, RoutedEventArgs e)
+        {
+            if (myWin == null)
+                myWin = new MyWindow();
+            myWin.Owner = this;
+            myWin.Show();
+            var location = New_Win.PointToScreen(new Point(0, 0));
+            myWin.Left = location.X + New_Win.Width;
+            myWin.Top = location.Y;
+
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            try
+            {
+            switch (feSource.Name)
+                {
+                    case "setBut":
+                        SetBut();
+                        break;
+                    case "retBut":
+                        RetBut();
+                        break;
+                }
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
